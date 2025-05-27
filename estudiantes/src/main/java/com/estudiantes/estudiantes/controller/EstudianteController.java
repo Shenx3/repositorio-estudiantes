2ec3be442eef19e056b6dbfa66355b99095ac77c
Shenx3 <bas.bravot@duocuc.cl>
2025-05-27 19:46:18 -0400

    Implementacion de Mapping en EstudianteController

--- estudiantes/src/main/java/com/estudiantes/estudiantes/controller/EstudianteController.java
+++ estudiantes/src/main/java/com/estudiantes/estudiantes/controller/EstudianteController.java
@@ -21,5 +21,88 @@ public class EstudianteController {
     @Autowired
     private CursoService cursoService;
 
+    @GetMapping // Listar Estudiantes
+    public ResponseEntity<List<Estudiante>> listarEstudiantes(){
+        List<Estudiante> estudiantes = estudianteService.findAll();
+        if(estudiantes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(estudiantes);
+    }
 
+    @PostMapping // Guardar Estudiantes
+    public ResponseEntity<Estudiante> guardarEstudiante(@RequestBody Estudiante estudiante) {
+        Estudiante estudiante1 = estudianteService.save(estudiante);
+        return ResponseEntity.status(HttpStatus.CREATED).body(estudiante1);
+    }
+
+    @GetMapping("/{id}") // Buscar por ID a Estudiantes
+    public ResponseEntity<Estudiante> buscarEstudiante(@PathVariable Integer id) {
+        try {
+            Estudiante estudiante = estudianteService.findById(id);
+            return ResponseEntity.ok(estudiante);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}") // Actualizar Estudiantes
+    public ResponseEntity<Estudiante> actualizarEstudiante(@PathVariable Integer id, @RequestBody Estudiante estudiante) {
+        try {
+            Estudiante estudianteActual = estudianteService.findById(id);
+            estudianteActual.setRunEstudiante(estudiante.getRunEstudiante());
+            estudianteActual.setNombre(estudiante.getNombre());
+            estudianteActual.setApellido(estudiante.getApellido());
+            estudianteActual.setEdad(estudiante.getEdad());
+            estudianteActual.setCurso(estudiante.getCurso());
+
+            estudianteService.save(estudianteActual);
+            return ResponseEntity.ok(estudianteActual);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}") // Eliminar Estudiantes
+    public ResponseEntity<Void> eliminarEstudiante(@PathVariable Long id) {
+        try {
+            estudianteService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // CURSOS
+
+    @PutMapping("/{id}/asignarcurso/{cursoId}") // Asignar Curso a Estudiante
+    public ResponseEntity<Estudiante> asignarCursoAEstudiante(
+            @PathVariable Integer id,
+            @PathVariable Integer cursoId) {
+
+        try {
+            Estudiante estudiante = estudianteService.findById(id);
+            Curso curso = cursoService.findById(cursoId);
+
+            estudiante.setCurso(curso);
+            estudianteService.save(estudiante);
+
+            return ResponseEntity.ok(estudiante);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{idEstudiante}/removercurso") // Remover Curso a Estudiante
+    public ResponseEntity<Void> removerEstudianteDeCurso(
+            @PathVariable Integer idEstudiante) {
+        try {
+            Estudiante estudiante = estudianteService.findById(idEstudiante);
+            estudiante.setCurso(null);
+            estudianteService.save(estudiante);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
